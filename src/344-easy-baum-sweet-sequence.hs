58a30a975018b28c4b251a7801348c214e46b12f
Giovanni Savini <giovanni.savini@stevini.it>
2017-12-16 00:01:24 +0100

    Update baumSweet function

--- src/344-easy-baum-sweet-sequence.hs
+++ src/344-easy-baum-sweet-sequence.hs
@@ -1,12 +1,10 @@
 import System.Environment
+import Data.List;
 
 main :: IO ()
 main = do
-  (arg:xs) <- getArgs
-  let numbers = [0..(read arg)]
-  let bin = map fromDecimalToBinary numbers
-  let res = map baumSweet bin
-  print res
+  (arg:_) <- getArgs
+  print (map (baumSweet . fromDecimalToBinary) [0..(read arg)])
 
 -- Thank you ehird: https://stackoverflow.com/a/9166342/6430775
 fromDecimalToBinary :: Int -> [Int]
@@ -16,19 +14,14 @@ fromDecimalToBinary n = reverse (fromDecimalToBinary' n)
 fromDecimalToBinary' :: Int -> [Int]
 fromDecimalToBinary' 0 = []
 fromDecimalToBinary' n = mod n 2 : fromDecimalToBinary' (div n 2)
-                       
+
+isOddNumberOfZeros :: [Int] -> Bool
+isOddNumberOfZeros number = case (length (filter (== 0) number)) `mod` 2 of
+                              0 -> False
+                              1 -> True
+
 baumSweet :: [Int] -> Int
 baumSweet [0] = 1
-baumSweet number = baumSweet' number 0
-
-baumSweet' :: [Int] -> Int -> Int
-baumSweet' [] i =
-  case mod i 2 of
-    0 -> 1
-    1 -> 0
-baumSweet' (x:xs) i =
-  case x of
-    0 -> baumSweet' xs (i + 1)
-    1 -> case mod i 2 of
-           0 -> baumSweet' xs 0
-           1 -> 0
+baumSweet digits = if any isOddNumberOfZeros (group digits)
+                   then 0
+                   else 1
